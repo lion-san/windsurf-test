75672cf3a8eed2ef32da3dc523ea46a196636613
Shinya Yokoi <i01210@hotmail.com>
2017-03-02 23:19:05 +0000

    footer copyright

--- wp-content/themes/himalayas/inc/functions.php
+++ wp-content/themes/himalayas/inc/functions.php
@@ -387,7 +387,8 @@ function himalayas_footer_copyright() {
 
 	$tg_link =  '<a href="'. 'http://themegrill.com/themes/himalayas' .'" target="_blank" title="'.esc_attr__( 'ThemeGrill', 'himalayas' ).'" rel="designer">'.__( 'ThemeGrill', 'himalayas') .'</a>';
 
-	$default_footer_value = '<span class="copyright-text">' . sprintf( __( 'Copyright &copy; %1$s %2$s.', 'himalayas' ), date( 'Y' ), $site_link ).' '.sprintf( __( 'Theme: %1$s by %2$s.', 'himalayas' ), 'Himalayas', $tg_link ).' '.sprintf( __( 'Powered by %s.', 'himalayas' ), $wp_link ) . '</span>';
+	//$default_footer_value = '<span class="copyright-text">' . sprintf( __( 'Copyright &copy; %1$s %2$s.', 'himalayas' ), date( 'Y' ), $site_link ).' '.sprintf( __( 'Theme: %1$s by %2$s.', 'himalayas' ), 'Himalayas', $tg_link ).' '.sprintf( __( 'Powered by %s.', 'himalayas' ), $wp_link ) . '</span>';
+	$default_footer_value = '<span class="copyright-text">' . sprintf( __( 'Copyright &copy; %1$s %2$s.', 'himalayas' ), date( 'Y' ), $site_link ).' ' . '</span>';
 
 	$himalayas_footer_copyright = '<div class="copyright">'.$default_footer_value.'</div>';
 	echo $himalayas_footer_copyright;
